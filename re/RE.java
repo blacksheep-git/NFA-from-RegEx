75a65c0710801fe13913c5760b944589ab5c5901
teo <m@teoo.io>
2021-12-05 16:19:49 -0800

    All methods done.

--- re/RE.java
+++ re/RE.java
@@ -10,7 +10,7 @@ import fa.nfa.NFAState;
  */
 public class RE implements REInterface {
 
-	private String re; //regular expression string - access assumed by other methods
+	private String re; 				//regular expression string - access assumed by other methods
 	private int stateCount = 0;
 
 	private final char STAR = '*';
@@ -21,7 +21,7 @@ public class RE implements REInterface {
 
 
 	/**
-	 * Construcor
+	 * Constructor
 	 * @param re regular expression as string type
 	 */
 	public RE(String re) {
@@ -29,31 +29,31 @@ public class RE implements REInterface {
 	}
 
 	/**
-	 * Returns the equivalent NFA for the RegEx used to instantiate RE object
-	 * @see re.REInterface#getNFA()
+	 * Recursive method. Returns the equivalent NFA for the RegEx used to instantiate RE object
+	 * @see REInterface#getNFA()
 	 */
 	@Override
-	public NFA getNFA() { //TODO: refactor code
+	public NFA getNFA() {
 		NFA term = term();
 
-		if (more() && peek() == OR) {
+		if (more() && peek() == OR) { 			//if there are still chars to consume in the regex string && the next char is an OR then need to create ONE NFA from both NFAs
 			eat(OR);
+			NFA nfa2 = getNFA();
+			NFAState nfastate = (NFAState) term.getStartState();   			// getStartState returns a State, so must be casted to NFAState type to call NFAState methods later
+			NFAState nfastate2 = (NFAState) nfa2.getStartState();   	// recursion returns the next
 
-			NFAState nfaState = (NFAState) term.getStartState();
-			NFAState nfaState2 = (NFAState) getNFA().getStartState();
+			term.addNFAStates(nfa2.getStates());
+			term.addAbc(nfa2.getABC());
 
-			term.addNFAStates(getNFA().getStates());
-			term.addAbc(getNFA().getABC());
-
-			String startState = Integer.toString(stateCount++);
+			String startState = String.valueOf(stateCount++);
 			term.addStartState(startState);
-			String finalState = Integer.toString(stateCount++);
+			String finalState = String.valueOf(stateCount++);
 			term.addFinalState(finalState);
 
-			term.addTransition(startState, E, nfaState.getName());
-			term.addTransition(startState, E, nfaState2.getName());
+			term.addTransition(startState, E, nfastate.getName());
+			term.addTransition(startState, E, nfastate2.getName());
 
-			for(State s:getNFA().getFinalStates()){
+			for(State s:nfa2.getFinalStates()){
 				NFAState state = (NFAState)s;
 				state.setNonFinal();
 				term.addTransition(state.getName(),E, finalState);
@@ -67,27 +67,26 @@ public class RE implements REInterface {
 	 * Inspired by https://matt.might.net/articles/parsing-regex-with-recursive-descent/
 	 * EX: base()
 	 */
-	private NFA base() { //TODO: refactor code
+	private NFA base() {
 		NFA nfa;
 
-		if(peek() == START_PRE){ //check redEx for specified precedence
-			eat(START_PRE);
-			nfa = getNFA();
-			eat(CLOSE_PRE);
+		if(peek() == START_PRE){ // If a ( is detected next, showing precedence then
+			eat(START_PRE);      // consume the open parenthesis
+			nfa = getNFA();		 // get the NFA for the reGex inside the () recursively
+			eat(CLOSE_PRE);		 // consume the closing parentheses
 		} else{
-			char c = next();
+			char c = next();	 // get the next char in the regex
 
-			nfa = new NFA();
+			nfa = new NFA();	 // since no precedence was detected, instantiate a new NFA to return
 
-			String startState = Integer.toString(stateCount++);
-			nfa.addStartState(startState);
+			String startState = String.valueOf(stateCount++);  //get the startState name from the number of states - will be used to set startState then a finalState of stateCount + 1
+			nfa.addStartState(startState);  //add the start state to the NFA
 
-			String finalState = Integer.toString(stateCount++);
-			nfa.addFinalState(finalState);
+			String finalState = String.valueOf(stateCount++);
+			nfa.addFinalState(finalState); //add this finalState to the NFA
 
-			nfa.addTransition(startState, c, finalState);
+			nfa.addTransition(startState, c, finalState);  //add a transition over char c read from the regEx from the Start state to the finalState
 		}
-
 		return nfa;
 	}
 
@@ -95,37 +94,44 @@ public class RE implements REInterface {
 	 * Inspired by https://matt.might.net/articles/parsing-regex-with-recursive-descent/
 	 * EX: factor()
 	 */
-	private NFA factor() {  //TODO: refactor code
-		NFA base = base() ;
-		while (more() && peek() == STAR) {
-			eat(STAR) ;
+	private NFA factor() {
+		NFA nfa = base(); //get the NFA over the next char OR if () then get that NFA
+		while (more() && peek() == STAR) { //until there are no additional *
+			eat(STAR); //consume the *
 
-			NFAState nfaState = (NFAState) base.getStartState();
-			for(State nfa : base.getFinalStates()){
-				base.addTransition(nfa.getName(), E, nfaState.getName());
+			NFAState nfaState = (NFAState) nfa.getStartState();
+			for(State s : nfa.getFinalStates()){ 					//for each final state of the nfa, add a transition over e to the StartState
+				nfa.addTransition(s.getName(), E, nfaState.getName());
 			}
 
-			String state = Integer.toString(stateCount++);
-			base.addStartState(state);
-			base.addFinalState(state);
-			base.addTransition(state, E, nfaState.getName());
+			String state = String.valueOf(stateCount);
+			nfa.addStartState(state); // state is now the start state, previous start state is just a regular state.
+			nfa.addFinalState(state); // state is a final state
+			nfa.addTransition(state, E, nfaState.getName()); //add transition over e, from the new state of name stateCount to the previously start state
+			stateCount++;
 		}
-		return base ;
+		return nfa ;
 	}
 
 	/**
 	 * Inspired by https://matt.might.net/articles/parsing-regex-with-recursive-descent/
 	 * EX: term()
 	 */
-	private NFA term() { //TODO: refactor code
-		NFA factor = new NFA();
+	private NFA term() {
+		NFA factor = new NFA(); //generate a new NFA to add states to
 		factor.addStartState(String.valueOf(stateCount++));
 		factor.addFinalState(String.valueOf(stateCount));
-		factor.addTransition(factor.getStartState().getName(), E, String.valueOf(stateCount));
+		factor.addTransition(factor.getStartState().getName(), E, String.valueOf(stateCount)); //establish transition over e from start to final state
 		stateCount++;
 
 		while (more() && peek() != CLOSE_PRE && peek() != OR) { //as suggested in https://matt.might.net/articles/parsing-regex-with-recursive-descent/
-			NFA nextFactor = factor();
+			NFA nextFactor;
+
+			if(peek() == STAR){
+				nextFactor = base();
+			} else{
+				nextFactor = factor();
+			}
 			factor.addAbc(nextFactor.getABC());
 
 			for(State s : factor.getFinalStates()){ //for each final state
